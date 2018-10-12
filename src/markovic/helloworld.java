2172e7eb7c20116a18e0403ae3a52ea09eded233
Philip Markovic <pmarkovic02@student.tgm.ac.at>
2018-10-12 12:07:55 +0200

    Zweite Version

--- src/markovic/helloworld.java
+++ src/markovic/helloworld.java
@@ -3,5 +3,8 @@ package markovic;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
